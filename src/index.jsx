32e89e63b0256cfcf3f9e7907787d0208021ab58
Harrison Howard <harrison.howard00707@gmail.com>
2022-01-02 12:52:54 +1000

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+console.log("Welcome to the console, what were you expecting to find?");
+
+const theme = createTheme({});
+
+ReactDOM.render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
