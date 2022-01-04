d2991b4eb977e305c3d816254f373cf58904cafd
Harrison Howard <harrison.howard00707@gmail.com>
2022-01-04 15:02:16 +1000

    Page component styles and layout

--- src/components/PageComponent.jsx
+++ src/components/PageComponent.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 function PageComponent(props) {
-    return <p>{props.name} Page</p>;
+    const classes = makeStyles((theme) => ({
+        "@global": {
+            body: {
+                background: props.colour || "white",
+                color: props.textColour || "black",
+            },
+        },
+        app: {
+            position: "relative",
+            display: "flex",
+            backgroundColor: !props.background && (props.colour || "white"),
+            width: "100%",
+            height: "100%",
+        },
+        background: {
+            position: "absolute",
+            backgroundImage: `url('${props.background}')`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            filter: "blur(8px)",
+            WebkitFilter: "blur(8px)",
+            width: "100%",
+            height: "100%",
+            "& body": {
+                background: "black",
+            },
+        },
+        container: {
+            margin: "5px",
+            width: "100%",
+        },
+        content: {
+            display: "flex",
+            justifyContent: "center",
+        },
+    }))();
+
+    return (
+        <>
+            {props.background && <div className={classes.background} />}
+            <div className={classes.app}>
+                <div className={classes.container}>
+                    <Typography variant="h1" fontSize={20}>
+                        {props.name}
+                    </Typography>
+                    <div className={classes.content}>{props.children}</div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default PageComponent;
--- src/routes/Home/HomePage.jsx
+++ src/routes/Home/HomePage.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import PageComponent from "../../components/PageComponent";
 
 function HomePage() {
-    return <PageComponent name="Home" />;
+    return (
+        <PageComponent
+            name="Home"
+            colour="#011a42"
+            textColour="white"
+            background="https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&w=1000&q=80"
+        >
+            Content
+        </PageComponent>
+    );
 }
 
 export default HomePage;
