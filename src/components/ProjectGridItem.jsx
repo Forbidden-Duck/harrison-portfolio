2b916ffc2b9588c55481717c333a6365d8e1169a
Harrison Howard <harrison.howard00707@gmail.com>
2022-02-28 14:24:18 +1000

    Fix width not being at default state of 100%

--- src/components/ProjectGridItem.jsx
+++ src/components/ProjectGridItem.jsx
@@ -6,6 +6,10 @@ function ProjectGridItem(props) {
     const gridItemRef = useRef(null);
 
     const classes = makeStyles((theme) => ({
+        gridItem: {
+            maxWidth: "400px",
+            width: "100%",
+        },
         gridItemHover: {
             animation: "$gridHover 0.5s ease-in-out forwards",
         },
@@ -14,17 +18,21 @@ function ProjectGridItem(props) {
         },
         "@keyframes gridHover": {
             "0%": {
+                maxWidth: "400px",
                 height: "260px",
             },
             "100%": {
+                maxWidth: "470px",
                 height: "280px",
             },
         },
         "@keyframes gridUnhover": {
             "0%": {
+                maxWidth: "470px",
                 height: `${gridItemRef.current?.offsetHeight || 280}px`,
             },
             "100%": {
+                maxWidth: "400px",
                 height: "260px",
             },
         },
@@ -34,13 +42,13 @@ function ProjectGridItem(props) {
 
     return (
         <Grid
-            className={
+            className={`${classes.gridItem} ${
                 !!hover
                     ? classes.gridItemHover
                     : hover === false
                     ? classes.gridItemUnhover
                     : ""
-            }
+            }`}
             ref={gridItemRef}
             item
         >
